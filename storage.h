d3f3508f6612988fcc252b22e0ec2f8acf536114
James Green <jxg4323@rit.edu>
2021-02-08 11:26:45 -0500

    PROJECT1 initialize header file for phase 1 of storage manager.

---
+++ storage.h
@@ -0,0 +1,27 @@
+/*
+Authors: Kelsey Dunn, Alex Frankel, James Green, Varnit Tewari
+Assigment: Phase 1
+Professor: Scott Johnson 
+*/
+#ifndef __STORAGE_H__
+#define __STORAGE_H__
+// TODO: confirm theses aren't in use w/ the system
+#define START_TABLE '/5' 
+#define END_TABLE '/6'
+
+int page_size;  // Page size should be a variable stored
+char * page_buffer;
+// beginning of file (first byte = page id)
+
+typedef struct table_info{
+	int table_id;
+	union record_item *** table;
+	int * page_ids;
+} table_info;
+
+typedef struct page_info{
+	int page_id;
+	union record_item *** page_data;
+} page_info;
+
+#endif
--- storagemanager.c
+++ storagemanager.c
@@ -1,2 +1,7 @@
+/*
+Authors: Kelsey Dunn, Alex Frankel, James Green, Varnit Tewari
+Assigment: Phase 1
+Professor: Scott Johnson 
+*/
 #include <storagemanager.h>
-
+#include <storage.h>
